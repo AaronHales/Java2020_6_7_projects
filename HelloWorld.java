5021a65f57b0349093ac4f9ae702217365fe0ea0
Aaron Hales <aaron.hales@tooeleschools.org>
2020-09-10 09:34:06 -0600

    Hello World added

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("Aaron Hales");
+		System.out.println("Senior");
+		System.out.println("GHS");
+		System.out.println("IDK");
+		System.out.println("What else do you want");
+		System.out.println("Why are we doing this");
+		System.out.println("Now there was only a few left");
+		System.out.println("Create a text file");
+		System.out.println("Bit code is the best code");
+		System.out.println("What is this for, there is an easier way, but i don't know the syntx, in html it is break");
+
+
+
+	}
+}
