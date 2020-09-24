d8dbc6f99a24369b02ec09a8c4e5c134f448e821
Aaron Hales <aaron.hales@tooeleschools.org>
2020-09-24 08:07:06 -0600

    chapter 4 lesson 2

---
+++ Chapter4/src/aaron/chapter4/lesson2/Lesson2exe.java
@@ -0,0 +1,40 @@
+package aaron.chapter4.lesson2;
+
+
+public class Lesson2exe {
+
+	public Lesson2exe() {
+		// variable declaration
+		int bank_account_total$; // 4 bytes
+		short num_of_hotdogs_i_can_eat; // 2 bytes
+		byte my_age; // 1 byte
+		long dream_total; // 8 bytes
+		
+		//fraction numbers
+		float my_gpa; // 4 bytes
+		double pie; // 8 bytes
+		
+		// boolean data types true or false
+		boolean answer; // 1 byte
+		
+		char letter; // 2 bytes
+		
+		bank_account_total$ = 600;
+		num_of_hotdogs_i_can_eat = 500;
+		my_age = 17;
+		dream_total = 200000000000L;
+		
+		my_gpa = 3.92F;
+		pie = 3.141579;
+		
+		letter = 'a';
+		answer = true;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
