3b6edf78cac8c07a45a5206b78ca0442e237c1c6
AaronHales <aaronmhales@gmail.com>
2020-09-27 12:53:48 -0600

    finished first variable activity

--- FirstVariables/src/aaron/hales/ch4/activity/FirstVariables.java
+++ FirstVariables/src/aaron/hales/ch4/activity/FirstVariables.java
@@ -1,3 +1,13 @@
+/*
+ <Java Programming>
+ <ch 4>
+ <Chapter 4 activity>
+ <First Variables>
+ <Aaron Hales>
+ <9/27/20>
+*/
+
+
 package aaron.hales.ch4.activity;
 
 public class FirstVariables {
@@ -7,8 +17,21 @@ public class FirstVariables {
 	}
 
 	public static void main(String[] args) {
+		
 		int my_integer = 34;
-		System
+		System.out.println("my_integer: " + my_integer);
+		
+		double my_double = 65.2;
+		System.out.println("my_double: " + my_double);
+		
+		boolean my_boolean = true;
+		System.out.println("my_boolean: " + my_boolean);
+		
+		char my_char = 'A';
+		System.out.println("my_char: " + my_char);
+		
+		String my_string = "Hello!";
+		System.out.println("my_string: " + my_string);
 
 	}
 
